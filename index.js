363a98acc7fd4c54db42dfdb9d647f7a4d3315d4
Yoandy Manuel <12760213+yoandymanuel@users.noreply.github.com>
2023-04-30 04:41:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port} or another port, how know it!`);
 });
